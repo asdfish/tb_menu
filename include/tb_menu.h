1adc027dac04c8e933967ed908f3debcea659224
asdfish <lgray3420@gmail.com>
2024-11-11 15:55:47 -0500

    added init & uninit

--- include/tb_menu.h
+++ include/tb_menu.h
@@ -1,3 +1,39 @@
 #ifndef TB_MENU_H
 #define TB_MENU_H
+
+#ifndef TBM_SUCCESS
+#define TBM_SUCCESS 0
+#endif
+#ifndef TBM_FAILURE_NULL_ARGS
+#define TBM_FAILURE_NULL_ARGS -1
+#endif
+
+#include <termbox2.h>
+#include <stdbool.h>
+
+struct TbMenuItem {
+  uintattr_t foreground;
+  uintattr_t foreground_reversed;
+  const char* contents;
+};
+struct TbMenu {
+  unsigned int x, y, width, height;
+  unsigned int camera, cursor;
+
+  uintattr_t background, background_reversed;
+
+  bool selected;                              // do not change
+  unsigned int selection;                     // do not change
+
+  struct MenuItem** items;                    // do not change
+  unsigned int items_length;                  // do not change
+
+  struct MenuItem** filtered_items;           // do not change
+  unsigned int filtered_items_length;         // do not change
+};
+
+extern int tb_menu_draw(struct TbMenu* menu);
+extern int tb_menu_init(struct TbMenu* menu);
+extern int tb_menu_uninit(struct TbMenu* menu);
+
 #endif
--- src/tb_menu.c
+++ src/tb_menu.c
@@ -1,8 +1,36 @@
 #include <tb_menu.h>
-#include <termbox2.h>
-#include <stdio.h>
 
-int i(void) {
-  printf("asdf");
-  return 0;
+int tb_menu_draw(struct TbMenu* menu) {
+  if(menu == NULL)
+    return TBM_FAILURE_NULL_ARGS;
+
+  return TBM_SUCCESS;
+}
+
+int tb_menu_init(struct TbMenu* menu) {
+  if(menu == NULL)
+    return TBM_FAILURE_NULL_ARGS;
+
+  menu->selected = false;
+  menu->selection = 0;
+
+  menu->items = NULL;
+  menu->items_length = 0;
+
+  menu->filtered_items = NULL;
+  menu->filtered_items_length = 0;
+  return TBM_SUCCESS;
+}
+
+int tb_menu_uninit(struct TbMenu* menu) {
+  if(menu == NULL)
+    return TBM_FAILURE_NULL_ARGS;
+
+  if(menu->filtered_items != NULL)
+    free(menu->filtered_items);
+
+  menu->filtered_items = NULL;
+  menu->filtered_items_length = 0;
+
+  return TBM_SUCCESS;
 }
